b29ba8190ed12169bbf17d4f2b9c30838c5d543d
r1cm3d <45370285+r1cm3d@users.noreply.github.com>
2024-07-24 09:30:52 -0300

    Create install.ps1

---
+++ cscfg/install.ps1
@@ -0,0 +1,12 @@
+# In order to be able to run scripts, you need to run "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser"
+
+$cfgPath = 'C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\core\cfg'
+
+Write-Output "`nCopying crosshairs files...`n"
+Copy-Item -Verbose -Force -Path "$PWD" -Filter ch*cfg -Destination $cfgPath
+
+Write-Output "`nCopying practice file...`n"
+Copy-Item -Verbose -Force -Path practice.cfg $cfgPath
+
+Write-Output "`nCopying autoexec file...`n"
+Copy-Item -Verbose -Force -Path autoexec.cfg $cfgPath
